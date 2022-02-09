f013baeee6996ace3a6cb319da6d7205cd0034a8
Ishaan-10 <b.ishaan10@gmail.com>
2022-02-10 00:02:15 +0530

    char sort

--- Assignment3/Q2.java
+++ Assignment3/Q2.java
@@ -1,3 +1,28 @@
 public class Q2 {
     
+    public static void main(String[] args) {
+        
+        String str = "zcedoepsnqalf";
+        sortString(str);
+
+    }
+
+    private static void sortString(String str) {
+
+        char ch[]= str.toCharArray();
+
+        for(int i=0;i<ch.length;i++){
+            for(int j=1;j<ch.length;j++){
+                if(ch[j]<ch[j-1]){
+                    char temp = (char)ch[j-1];
+                    ch[j-1]=(char)ch[j];
+                    ch[j]=(char)temp;
+                }
+            }
+        }
+        for(char c:ch){
+            System.out.print(c);
+        }
+
+    }
 }
